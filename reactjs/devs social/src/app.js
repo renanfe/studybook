a5c29da3076b79a4c819a82ed68c5bc411b0d3f6
Renan Felipe Teixeira <42209986+renanfe@users.noreply.github.com>
2021-03-13 21:42:50 -0300

    New comment

--- reactjs/devs social/src/app.js
+++ reactjs/devs social/src/app.js
@@ -4,7 +4,7 @@ import React, { useEffect, useState } from 'react';
 import Sidebar from './components/sidebar/sidebar';
 import Main from './components/main/main';
 import api from './services/api';
-
+//New Comment
 export default function App() {
 
   const [devs, setDevs] = useState([]);
